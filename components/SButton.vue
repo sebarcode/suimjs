e72159162ea039ebefd6f2be711ee46479663305
DESKTOP-LQLVVOI\LENOVO <rosyid@kanosolution.com>
2024-02-16 13:28:07 +0700

    [F] component tooltip

--- components/SButton.vue
+++ components/SButton.vue
@@ -1,27 +1,20 @@
 <template>
-  <div class="has_suim_tooltip relative flex flex-col items-center group">
-    <button
-      class="flex gap-1 btn items-center"
-      :disabled="disableBtn"
-      @click="clickBtn"
-    >
-      <mdicon width="14" :name="icon" v-if="icon != ''" />
-      <div class="mt-0" v-if="label != ''">{{ label }}</div>
-    </button>
-    <div
-      v-if="isTooltip"
-      class="suim_tooltip absolute flex flex-col items-center group-hover:flex rounded"
-    >
-      <span
-        class="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg"
-        >{{ label ? label : tooltip }}</span
+  <s-tooltip :no-tooltip="noTooltip" :tooltip="label ? label : tooltip">
+    <template #content>
+      <button
+        class="flex gap-1 btn items-center"
+        :disabled="disableBtn"
+        @click="clickBtn"
       >
-      <div class="rotate-45 bg-black arrow-bottom"></div>
-    </div>
-  </div>
+        <mdicon width="14" :name="icon" v-if="icon != ''" />
+        <div class="mt-0" v-if="label != ''">{{ label }}</div>
+      </button>
+    </template>
+  </s-tooltip>
 </template>
 
 <script setup>
+import STooltip from "./STooltip.vue";
 import { getCurrentInstance } from "vue";
 import { reactive } from "vue";
 import { computed } from "vue";
@@ -32,7 +25,7 @@ const props = defineProps({
   disabled: { type: Boolean, default: false },
   disableWhenClicked: { type: Boolean, default: false },
   tooltip: { type: String, default: "tooltip" },
-  isTooltip: { type: String, default: true },
+  noTooltip: { type: String, default: false },
 });
 
 const data = reactive({
@@ -71,25 +64,5 @@ const disableBtn = computed({
 
   min-height: 20px !important;
   height: 2rem;
-}
-
-.suim_tooltip {
-  visibility: hidden;
-  background-color: #333333;
-  top: -50px;
-  z-index: 100;
-  width: max-content;
-}
-.has_suim_tooltip:hover .suim_tooltip {
-  visibility: visible;
-}
-.arrow-bottom {
-  content: " ";
-  position: absolute;
-  bottom: -8px;
-  border-top: 8px solid #333333;
-  border-right: 8px solid transparent;
-  border-left: 8px solid transparent;
-  border-bottom: none;
 }
 </style>
