cfbd341dd26d636788e4f14acf7536c2d8561577
ariefdarmawan <ariefda@hotmail.com>
2026-08-29 05:54:29 +0700

    raise modal overlay above all layers

--- components/SModal.vue
+++ components/SModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="modal_fullbg" v-if="data.displayModal">&nbsp;</div>
 
-    <div class="w-full h-full z-50 flex items-center justify-center" style="position: fixed; inset: 0;" v-if="data.displayModal">
+    <div class="w-full h-full flex items-center justify-center" style="position: fixed; inset: 0; z-index: 9999;" v-if="data.displayModal">
         <div class="modal_container">
             <slot name="title">
                 <div class="modal_title bg-primary text-white" v-if="!hideTitle">
@@ -91,6 +91,7 @@ defineExpose({
 .modal_fullbg {
     position: fixed;
     inset: 0;
+    z-index: 9998;
     width: 100%;
     height: 100%;
     background: black;
